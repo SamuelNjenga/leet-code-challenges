29157ce6ec3a3d5929d9a7f22b11355ef2f5f581
Samuel Njenga <snjenga704@gmail.com>
2020-06-20 21:13:55 +0300

    Added RemoveKDigits.java problem

---
+++ src/RemoveKDigits.java
@@ -0,0 +1,30 @@
+public class RemoveKDigits {
+
+    public String removeKdigits(String num, int k) {
+        if(num.length()==k)
+            return "0";
+
+        StringBuilder sb = new StringBuilder(num);
+        for(int j=0; j<k; j++){
+            int i=0;
+            while(i<sb.length()-1&&sb.charAt(i)<=sb.charAt(i+1)){
+                i++;
+            }
+            sb.delete(i, i+1);
+        }
+
+        //remove leading 0's
+        while (sb.length() > 1 && sb.charAt(0)=='0')
+            sb.delete(0,1);
+
+        if(sb.length()==0){
+            return "0";
+        }
+
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        RemoveKDigits obj = new RemoveKDigits();
+        System.out.println(obj.removeKdigits("234",2));
+    }
+}
